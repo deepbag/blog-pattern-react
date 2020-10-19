e2bc521bc1d755bd5887d804eb43c7721188caaa
deepbag <deepbag92@gmail.com>
2020-10-19 14:29:26 +0530

    Updates

---
+++ precache-manifest.15aa399b0346700cf05a29f670021707.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f4475a706c816c5bf9fd636fe2d994",
+    "url": "/blog-pattern-react/index.html"
+  },
+  {
+    "revision": "d89d30b0df4d601c9850",
+    "url": "/blog-pattern-react/static/css/main.a4757f48.chunk.css"
+  },
+  {
+    "revision": "2cbd3ad6dffeb8d1ebd6",
+    "url": "/blog-pattern-react/static/js/2.7f57d288.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/blog-pattern-react/static/js/2.7f57d288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d89d30b0df4d601c9850",
+    "url": "/blog-pattern-react/static/js/main.37cbc371.chunk.js"
+  },
+  {
+    "revision": "b2dd8012429bd48bf961",
+    "url": "/blog-pattern-react/static/js/runtime-main.77e827b1.js"
+  }
+]);
